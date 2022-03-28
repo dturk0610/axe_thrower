e94b6e53fd499dfee34d9f9fe194f22e787b1c59
dakota.turk <dturk0610@gmail.com>
2022-03-27 23:14:40 -0400

    lighting was working, had to change shader values

--- AxeThrower/game.js
+++ AxeThrower/game.js
@@ -32,7 +32,7 @@ var myShaderProgram;
 var h, w, invh, invw;
 var myScene;
 
-var floorShader;
+var floorShader, lightShader;
 
 function initGL(){
     var canvas = document.getElementById( "gl-canvas" );
@@ -65,7 +65,8 @@ function setupGL(){
     gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
     MeshRenderer.setGL(gl);
     
-    floorShader =  initShaders( gl, "vertex-shader-quad", "fragment-shader" );
+    floorShader = initShaders( gl, "vertex-shader-quad", "fragment-shader" );
+    lightShader = initShaders( gl, "vertex-phrongLighting", "fragment-phrongLighting" );
 }
 
 
@@ -87,11 +88,7 @@ function setupScene(){
         this.gl.useProgram( this.shaderProgram );
 
         var mesh = this.gameObject.mesh;
-
-
-        var numVerts = mesh.numVertices;
         var numTriangles = mesh.numTriangles;
-
         var verts = mesh.getVertices();
         var indexList = mesh.getFaces();
 
@@ -114,39 +111,124 @@ function setupScene(){
         this.gl.drawElements( this.gl.TRIANGLES, 3 * numTriangles, this.gl.UNSIGNED_SHORT, 0 )
     }
 
+    var lightObjRenderSetup = function( shaderProgram, meshRenderer ){
+        var gl = this.gl;
+        meshRenderer.indexBuffer = gl.createBuffer();
+        meshRenderer.verticesBuffer = gl.createBuffer();
+        meshRenderer.vertexPosition = gl.getAttribLocation( shaderProgram, "vertexPosition");
+        meshRenderer.normalsBuffer = gl.createBuffer();
+        meshRenderer.vertexNormalPointer = gl.getAttribLocation( shaderProgram, "nv" )
+        meshRenderer.worldMatLocation = gl.getUniformLocation( shaderProgram, "worldMat" );
+        meshRenderer.worldMatInverseLocation = gl.getUniformLocation( shaderProgram, "worldMatInverse" );
+        meshRenderer.perspectiveProjectionMatricLocation = gl.getUniformLocation( shaderProgram, "projectMat" );
+        meshRenderer.p0loc = gl.getUniformLocation( shaderProgram, "p0" );
+        meshRenderer.Ialoc = gl.getUniformLocation( shaderProgram, "Ia" );
+        meshRenderer.Idloc = gl.getUniformLocation( shaderProgram, "Id" );
+        meshRenderer.Isloc = gl.getUniformLocation( shaderProgram, "Is" );
+        meshRenderer.abcDist = gl.getUniformLocation( shaderProgram, "abcDist" );
+        meshRenderer.kaloc = gl.getUniformLocation( shaderProgram, "ka" );
+        meshRenderer.kdloc = gl.getUniformLocation( shaderProgram, "kd" );
+        meshRenderer.ksloc = gl.getUniformLocation( shaderProgram, "ks" );
+        meshRenderer.alphaloc = gl.getUniformLocation( shaderProgram, "alpha" );
+        meshRenderer.colUniformLocation = gl.getUniformLocation( shaderProgram, "col" );
+    }
+
+    var lightObjReneder = function(){
+        var gl = this.gl;
+        gl.useProgram( this.shaderProgram );
+
+        var mesh = this.gameObject.mesh;
+        var numTriangles = mesh.numTriangles;
+        var verts = mesh.getVertices();
+        var indexList = mesh.getFaces();
+
+        gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer );
+        gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexList), gl.STATIC_DRAW );
+
+        gl.bindBuffer( gl.ARRAY_BUFFER, this.verticesBuffer );
+        gl.bufferData( gl.ARRAY_BUFFER, flatten(verts), gl.STATIC_DRAW );
+
+        gl.vertexAttribPointer( this.vertexPosition, 4, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( this.vertexPosition );
+
+        //var faceNormals = mesh.faceNormals;
+        var vertexNormals = mesh.vertexNormals;
+        
+        gl.bindBuffer( gl.ARRAY_BUFFER, this.normalsBuffer );
+        gl.bufferData( gl.ARRAY_BUFFER, flatten( vertexNormals ), gl.STATIC_DRAW );
+
+        gl.vertexAttribPointer( this.vertexNormalPointer, 3, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( this.vertexNormalPointer );
+
+        var cam = Scene.mainCam;
+
+        var projectionMat = cam.projectionMat;
+        var viewMat = cam.viewMat;
+        var viewProjectMat = Matrix.mult4x4( projectionMat, viewMat );
+        var worldMat = this.gameObject.worldMat;
+        var worldMatInverse = Matrix.inverseM4x4( this.gameObject.worldMat );
+
+        gl.uniformMatrix4fv( this.worldMatLocation, false, flatten( worldMat ) );
+        gl.uniformMatrix4fv( this.worldMatInverseLocation, false, flatten( worldMatInverse ) );
+        gl.uniformMatrix4fv( this.perspectiveProjectionMatricLocation, false, flatten( viewProjectMat ) );
+        
+        var color = this.gameObject.color;
+        gl.uniform4f( this.colUniformLocation, color[0], color[1], color[2], color[3] );
+        
+        
+        gl.uniform3f( this.kaloc, 0.5, 0.5, 0.5 );
+        gl.uniform3f( this.kdloc, 0.5, 0.5, 0.5 );
+        gl.uniform3f( this.ksloc, 1.0, 1.0, 1.0 );
+        
+        gl.uniform1f( this.alphaloc, 4.0 );
+        
+        var posX = cam.transform.position[0];
+        var posY = cam.transform.position[1] * 3;
+        var posZ = cam.transform.position[2];
+
+        gl.uniform3f( this.p0loc, posX, posY, posZ );
+        
+        gl.uniform3f( this.Ialoc, 0.1, 0.1, 0.1 );
+        gl.uniform3f( this.Idloc, 0.8, 0.8, 0.5 );
+        gl.uniform3f( this.Isloc, 0.8, 0.8, 0.8 );
+        gl.uniform3f( this.abcDist, .05, 0.0, 0.0 );
+        
+        gl.drawElements( gl.TRIANGLES, 3 * numTriangles, gl.UNSIGNED_SHORT, 0 );
+    }
+
     var floor = new Quad( 10, 10, vec4( 0, 0, 0, 1), new Quat( 0, 0, 0, 1 ), vec3( 1, 1, 1 ) );
     floor.meshRenderer = new MeshRenderer( floor, floorShader, baseObjRenderSetup, baseObjReneder );
     floor.color = vec4( 1, 0, 0, 1 );
     myScene.addObject( floor );
 
     var wall1 = new Quad( 10, 2, vec4(  0, 1, 5, 1), Quat.fromAxisAndAngle( vec3( 1, 0, 0 ), 90 ), vec3( 1, 1, 1 ) );
-    wall1.meshRenderer = new MeshRenderer( wall1, floorShader, baseObjRenderSetup, baseObjReneder );
+    wall1.meshRenderer = new MeshRenderer( wall1, lightShader, lightObjRenderSetup, lightObjReneder );
     wall1.color = vec4( 1, 1, 0, 1 );
     myScene.addObject( wall1 );
 
     var yTurn = Quat.fromAxisAndAngle( vec3( 0, 1, 0 ), 90 );
     var zTurn = Quat.fromAxisAndAngle( vec3( 0, 0, -1 ), 90 );
     var wall2 = new Quad( 10, 2, vec4(  5, 1, 0, 1), Quat.mult( yTurn, zTurn ), vec3( 1, 1, 1 ) );
-    wall2.meshRenderer = new MeshRenderer( wall2, floorShader, baseObjRenderSetup, baseObjReneder );
+    wall2.meshRenderer = new MeshRenderer( wall2, lightShader, lightObjRenderSetup, lightObjReneder );
     wall2.color = vec4( 0, 1, 0, 1 );
     myScene.addObject( wall2 );
 
     var wall3 = new Quad( 10, 2, vec4(  0, 1, -5, 1), Quat.fromAxisAndAngle( vec3( -1, 0, 0 ), 90 ), vec3( 1, 1, 1 ) );
-    wall3.meshRenderer = new MeshRenderer( wall3, floorShader, baseObjRenderSetup, baseObjReneder );
+    wall3.meshRenderer = new MeshRenderer( wall3, lightShader, lightObjRenderSetup, lightObjReneder );
     wall3.color = vec4( 0, 0, 1, 1 );
     myScene.addObject( wall3 );
 
     var yTurn = Quat.fromAxisAndAngle( vec3( 0, 1, 0 ), 90 );
     var zTurn = Quat.fromAxisAndAngle( vec3( 0, 0, 1 ), 90 );
     var wall4 = new Quad( 10, 2, vec4( -5, 1, 0, 1), Quat.mult( yTurn, zTurn ), vec3( 1, 1, 1 ) );
-    wall4.meshRenderer = new MeshRenderer( wall4, floorShader, baseObjRenderSetup, baseObjReneder );
+    wall4.meshRenderer = new MeshRenderer( wall4, lightShader, lightObjRenderSetup, lightObjReneder );
     wall4.color = vec4( 0.8, 0.2, 1, 1 );
     myScene.addObject( wall4 );
 
 
-    var chair = new GameObject( "chair", vec4( 0, .5, -4, 1 ), Quat.identity, vec3( .02, .02, .02 ) );
+    var chair = new GameObject( "chair", vec4( 0, .45, -4, 1 ), Quat.identity, vec3( .02, .02, .02 ) );
     chair.mesh = new Mesh( getChairVertices(), getChairFaces() );
-    chair.meshRenderer = new MeshRenderer( chair, floorShader, baseObjRenderSetup, baseObjReneder );
+    chair.meshRenderer = new MeshRenderer( chair, lightShader, lightObjRenderSetup, lightObjReneder );
     chair.color = vec4( 0.8, 0.2, .4, 1 );
     myScene.addObject( chair );
 
