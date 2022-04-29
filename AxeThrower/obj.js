4be52084b431dab6d7592ed3c8ec1741b0873acf
dakota.turk <dturk0610@gmail.com>
2022-04-28 23:54:09 -0400

    obj parse

--- AxeThrower/obj.js
+++ AxeThrower/obj.js
@@ -1,16 +1,36 @@
 class OBJ{
 
-    static knownKeywords = [];
+    static knownKeywords = [ "#", "v", "vt", "g", "f", "mtllib", "o", "vn", "usemtl", "s" ];
 
     static parse( objText ){
+
+        var verts = [];
+        var textVerts = [];
+
         var parsedText = objText.replaceAll('\r', '').split("\n");
-        for ( var i = 0; i < parsedText.count; i++ ){
-            if ( parsedText[i] == '' ) continue;
+        for ( var i = 0; i < parsedText.length; i++ ){
+            if ( parsedText[i] == "" ) continue;
             var lineParse = parsedText[i].split(' ');
-            console.log(lineParse);
             if ( !OBJ.knownKeywords.includes( lineParse[0] ) ) OBJ.knownKeywords.push( lineParse[0] );
+
+            switch( lineParse[0] ){
+                case "v":
+                    lineParse.shift();
+                    verts.push( new Vector3( lineParse[0], lineParse[1], lineParse[2] ) );
+                    break;
+                case "vt":
+                    lineParse.shift();
+                    textVerts.push( new Vector2( lineParse[0], lineParse[1] ) );
+                    break;
+                case "f":
+                    lineParse.shift();
+                    for ( var i = 0; i < lineParse.length )
+                    break;
+            }
         }
-        console.log(  );
+        console.log( verts );
+        console.log( textVerts );
+        return "Yes";
     }
 
     static readFile( fileName ){
@@ -18,6 +38,7 @@ class OBJ{
 
         http.open( "GET", fileName, false );
         http.send();
+
         return OBJ.parse(http.responseText);
     }
 
